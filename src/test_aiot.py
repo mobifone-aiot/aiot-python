6013670de2057511dc58d2786b1af4877118da84
thanhdaon <thanhdaon89@gmail.com>
2021-11-09 15:34:17 +0700

    update sdk for auth

--- src/test_aiot.py
+++ src/test_aiot.py
@@ -1,6 +1,21 @@
 from aiot import Client
+import json
+
+gatewayAddr = "http://10.16.150.132"
+validEmail = "testsdk@mobifone.vn"
+validPassword = "12345678"
+invalidPassword = "sda21j3h123"
 
 
-def test_aiot_hello():
-    client = Client()
-    assert client.hello() == "hello world!"
+def test_token():
+    client = Client(gatewayAddr)
+    token = client.token(validEmail, validPassword)
+    ok = client.token_verify(token)
+    assert(ok)
+
+
+def test_user_profile():
+    client = Client(gatewayAddr)
+    token = client.token(validEmail, validPassword)
+    profile = client.user_profile(token)
+    print(profile)
